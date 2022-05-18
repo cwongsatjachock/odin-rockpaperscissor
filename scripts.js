29688e6a70af40f0ae01f45b53611777fb6b5a0a
Victor Wongsatjachock <victorwongsatjachock@gmail.com>
2022-05-17 18:33:09 -0700

    update playRound

--- scripts.js
+++ scripts.js
@@ -1,3 +1,8 @@
+let playerSelection, 
+    result,
+    playerScore = 0,
+    comScore = 0; 
+
 function computerPlay() {
     switch(Math.floor(Math.random() * 3)) {
         case 0:
@@ -11,33 +16,38 @@ function computerPlay() {
 
 function playRound( playerSelection, computerSelection ) {
     playerSelection = playerSelection.toLowerCase();
-    switch (playerSelection) {
-        case 'rock':
-            switch (computerSelection) {
-                case 'rock':
-                    return 'You Tie! Rock equals Rock';
-                case 'paper':
-                    return 'You Lose! Paper beats Rock';
-                case 'scissor':
-                    return 'You Win! Rock beats Scissor';
-            }
-        case 'paper':
-            switch (computerSelection) {
-                case 'rock':
-                    return 'You Win! Paper beats Rock';
-                case 'paper':
-                    return 'You Tie! Paper equals Paper';
-                case 'scissor':
-                    return 'You Lose! Scissor beats Paper';
-            }
-        case 'scissor':
-            switch (computerSelection) {
-                case 'rock':
-                    return 'You Lose! Rock beats Scissor';
-                case 'paper':
-                    return 'You Win! Scissor beats Paper';
-                case 'scissor':
-                    return 'You Tie! Scissor equals Scissor';
-            }
+    if(playerSelection == 'rock' && computerSelection == 'scissor' || playerSelection == 'paper' && computerSelection == 'rock' || playerSelection == 'scissor' && computerSelection == 'paper') {
+        return `You Win! ${playerSelection} beats ${computerSelection}`;
+    } else if ( playerSelection == computerSelection )
+    {
+        return `You Tie! ${playerSelection} equals ${computerSelection}`;
+    } else {
+        return `You Lose! ${computerSelection} beats ${playerSelection}`;
     }
 }
+
+for (let i = 0; i < 5; i++) {
+    playerSelection = prompt("Please enter your choice of rock, paper, or scissor: ", "");
+    result = playRound(playerSelection, computerPlay());
+    console.log(result);
+    console.log(result.charAt(4));
+    switch(result.charAt(4)) {
+        case 'W':
+            playerScore += 1;
+            break;
+        case 'L':
+            comScore += 1;
+            break;
+    }
+
+    if(i == 4){
+        if (playerScore > comScore) {
+            console.log('Player Win!');
+        } else if (comScore > playerScore) {
+            console.log('Computer Win!');
+        } else {
+            console.log('Tie!')
+        }
+    }
+}
+
